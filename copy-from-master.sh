cb2574967519d8a3c9398c0318eaba2d615c10aa
emanuele <emanuele.raineri@cnag.crg.eu>
2022-08-17 16:09:56 +0200

    copy from master

---
+++ copy-from-master.sh
@@ -0,0 +1,9 @@
+git show master:clustering.c > clustering.c
+git show master:cvlr-cluster.c > cvlr-cluster.c
+git show master:cvlr-meth-of-bam.c > cvlr-meth-of-bam.c
+git show master:cvlrlib.c > cvlrlib.c
+git show master:tree.c > tree.c
+git show master:binomlib.ml > binomlib.ml
+git show master:cvlr-stats.ml > cvlr-stats.ml
+git show master:cvlr-stats.py > cvlr-stats.py
+git show master:README.md > README.md
