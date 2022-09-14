70557c6a6715851e734d56e8b8625a9c8068f923
Emanuele Raineri <emanuele.raineri@gmail.com>
2022-09-14 11:02:04 +0200

    first commit

--- copy-from-master.sh
+++ copy-from-master.sh
@@ -1,9 +1,18 @@
 git show master:clustering.c > clustering.c
+git add clustering.c
 git show master:cvlr-cluster.c > cvlr-cluster.c
+git add cvlr-cluster.c
 git show master:cvlr-meth-of-bam.c > cvlr-meth-of-bam.c
+git add cvlr-meth-of-bam.c
 git show master:cvlrlib.c > cvlrlib.c
+git add cvlrlib.c
 git show master:tree.c > tree.c
+git add tree.c
 git show master:binomlib.ml > binomlib.ml
+git add binomlib.ml
 git show master:cvlr-stats.ml > cvlr-stats.ml
+git add cvlr-stats.ml
 git show master:cvlr-stats.py > cvlr-stats.py
+git add cvlr-stats.py
 git show master:README.md > README.md
+git add README.md
