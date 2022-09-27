16b1b1f6c5a1470c5df959ecb4ba59a9d71f1d51
Marc Dabad <marc.dabad@cnag.crg.cat>
2022-09-27 16:04:36 +0200

    cc

--- makefile-conda.mk
+++ makefile-conda.mk
@@ -4,10 +4,10 @@ LIB=$(PREFIX)/lib/libhts.a -lpthread -lz -lcurl -lcrypto -lm -lbz2 -llzma
 all: cvlr-meth-of-bam cvlr-cluster 
 
 cvlr-cluster: cvlr-cluster.c clustering.c
-	$(CC) -Wall -pedantic -o $@ $< -lm			
+	gcc -Wall -pedantic -o $@ $< -lm			
 
 cvlr-meth-of-bam: cvlr-meth-of-bam.c cvlrlib.c tree.c
-	$(CC) -o $@ $(INCLUDE) -L$(PREFIX)/lib -fPIC  $< $(LIB)
+	gcc -o $@ $(INCLUDE) -L$(PREFIX)/lib -fPIC  $< $(LIB)
 
 
 
